db00a52bfd8e10825282d0701a10bfef78614661
admin <rajnish.kahar1996@gmail.com>
2020-01-23 12:06:52 +0530

    REFACTORED: Operations In Dictionary

--- arithmetic_operation.sh
+++ arithmetic_operation.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 read -p "Enter three Numbers: " a b c
+declare -A Operations 
 echo "$a $b $c";
-operation1=`echo "scale=2;($a+($b*$c))" | bc`;
-operation2=`echo "scale=2;(($a*$b)+$c)" | bc`;
-operation3=`echo "scale=2;($c+($a+$b))" | bc`;
-operation4=`echo "scale=2;(($a%$b)+$c)" | bc`;
-printf "Value of opr1: $operation1\nValue of opr2: $operation2\nValue of opr3: $operation3\nValue of opr4: $operation4\n"
+Operations["operation1"]="`echo "scale=2;($a+($b*$c))" | bc`"
+Operations["operation2"]="`echo "scale=2;(($a*$b)+$c)" | bc`"
+Operations["operation3"]="`echo "scale=2;($c+($a/$b))" | bc`"
+Operations["operation4"]="`echo "scale=2;(($a%$b)+$c)" | bc`"
+printf  "Value of opr1: ${Operations[operation1]}\nValue of opr2: ${Operations[operation2]}\n""Value of opr3: ${Operations[operation3]}\n""Value of opr4: ${Operations[operation4]}\n";
