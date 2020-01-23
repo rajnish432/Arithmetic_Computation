e1bb6f416589a0345c92ef5da2d8ef4b7dc7862a
admin <rajnish.kahar1996@gmail.com>
2020-01-23 14:42:35 +0530

    ADDED: Descending Sorting

--- arithmetic_operation.sh
+++ arithmetic_operation.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash -x 
 
 read -p "Enter three Numbers: " a b c
 declare -A Operations 
@@ -12,9 +12,28 @@ function dictionary_to_array()
 	done
 }
 
-Operations["operation1"]="`echo "scale=2;($a+($b*$c))" | bc`"
-Operations["operation2"]="`echo "scale=2;(($a*$b)+$c)" | bc`"
-Operations["operation3"]="`echo "scale=2;($c+($a/$b))" | bc`"
-Operations["operation4"]="`echo "scale=2;(($a%$b)+$c)" | bc`"
+function descending_Sort()
+{
+for (( i=0; i<$length; i++ ))
+do
+   for (( j=i+1; j<=$length; j++))
+   do
+      if [[ ${arr[i]%%.*} -lt ${arr[j]%%.*} ]]
+      then
+      temp=${arr[i]};
+      arr[i]=${arr[j]};
+      arr[j]=$temp;
+      fi
+   done
+done
+}
+
+Operations["operation1"]="`echo "scale=2;($a+$b*$c)" | bc`"
+Operations["operation2"]="`echo "scale=2;($a*$b+$c)" | bc`"
+Operations["operation3"]="`echo "scale=2;($c+$a/$b)" | bc`"
+Operations["operation4"]="`echo "scale=2;($a%$b+$c)" | bc`"
 dictionary_to_array 
+length=${#arr[@]}
+descending_Sort
+echo "Array in Descending order:"
 printf "value of opr1: ${arr[0]}\nvalue of opr2: ${arr[1]}\nvalue of opr3: ${arr[2]}\nvalue of opr4: ${arr[3]}\n"
