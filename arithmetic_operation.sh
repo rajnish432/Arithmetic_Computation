a940208fa44d59a04d7d36e08e4859f7d88c5d1c
admin <rajnish.kahar1996@gmail.com>
2020-01-23 11:07:44 +0530

    ADDED: Fourth Operation

--- arithmetic_operation.sh
+++ arithmetic_operation.sh
@@ -5,4 +5,5 @@ echo "$a $b $c";
 operation1=`echo "scale=2;($a+($b*$c))" | bc`;
 operation2=`echo "scale=2;(($a*$b)+$c)" | bc`;
 operation3=`echo "scale=2;($c+($a+$b))" | bc`;
-printf "Value of opr1: $operation1\nValue of opr2: $operation2\nValue of opr3: $operation3\n"
+operation4=`echo "scale=2;(($a%$b)+$c)" | bc`;
+printf "Value of opr1: $operation1\nValue of opr2: $operation2\nValue of opr3: $operation3\nValue of opr4: $operation4\n"
