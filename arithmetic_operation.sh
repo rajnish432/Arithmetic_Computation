ef2fc958be47ac026115b9727e7eab7ca97b59ee
admin <rajnish.kahar1996@gmail.com>
2020-01-23 14:12:18 +0530

    ADDED: Values in Array

--- arithmetic_operation.sh
+++ arithmetic_operation.sh
@@ -1,10 +1,20 @@
-#! /bin/bash
+#! /bin/bash 
 
 read -p "Enter three Numbers: " a b c
 declare -A Operations 
 echo "$a $b $c";
+
+function dictionary_to_array()
+{
+	for (( i=0; i<4; i++ ))
+	do
+	arr[i]=${Operations[operation$((i+1))]}
+	done
+}
+
 Operations["operation1"]="`echo "scale=2;($a+($b*$c))" | bc`"
 Operations["operation2"]="`echo "scale=2;(($a*$b)+$c)" | bc`"
 Operations["operation3"]="`echo "scale=2;($c+($a/$b))" | bc`"
 Operations["operation4"]="`echo "scale=2;(($a%$b)+$c)" | bc`"
-printf  "Value of opr1: ${Operations[operation1]}\nValue of opr2: ${Operations[operation2]}\n""Value of opr3: ${Operations[operation3]}\n""Value of opr4: ${Operations[operation4]}\n";
+dictionary_to_array 
+printf "value of opr1: ${arr[0]}\nvalue of opr2: ${arr[1]}\nvalue of opr3: ${arr[2]}\nvalue of opr4: ${arr[3]}\n"
