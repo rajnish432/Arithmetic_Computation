561dd7ab6e0b057e7b98922ec61caf76bdaf01a8
admin <rajnish.kahar1996@gmail.com>
2020-01-23 10:55:51 +0530

    ADDED: Third Operation

--- arithmetic_operation.sh
+++ arithmetic_operation.sh
@@ -4,4 +4,5 @@ read -p "Enter three Numbers: " a b c
 echo "$a $b $c";
 operation1=`echo "scale=2;($a+($b*$c))" | bc`;
 operation2=`echo "scale=2;(($a*$b)+$c)" | bc`;
-printf "Value of opr1: $operation1\nValue of opr2: $operation2\n"
+operation3=`echo "scale=2;($c+($a+$b))" | bc`;
+printf "Value of opr1: $operation1\nValue of opr2: $operation2\nValue of opr3: $operation3\n"
